37381851fb9066a7ec0a43d4dc7ff3aeb5d9cacc
James Ferguson <ncjamesferguson@gmail.com>
2017-12-05 18:37:01 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        name: "Lord capulet"
+        name: "Lord Capulet"
         age: "51"
         },
       :matriarch => {
